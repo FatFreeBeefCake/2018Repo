cf488d43bd570998a29ae134701438463e2b0b3a
Unknown <stheadman1@hotmail.com>
2018-03-06 12:41:04 -0700

    Test Commit

---
+++ CDGameStuff/Assets/Scriptsss/ClickEventsJump.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickEventsJump : MonoBehaviour {
+
+    public Animator Animations;
+    public Box box;
+
+    public string anim;
+
+    void Start()
+    {
+        anim = box.Animation;
+    }
+    private void OnMouseDown()
+    {
+        if (GameObject.FindWithTag("Jump"))
+        {
+            Animations.SetBool(anim , true);
+        }
+    }
+
+}
