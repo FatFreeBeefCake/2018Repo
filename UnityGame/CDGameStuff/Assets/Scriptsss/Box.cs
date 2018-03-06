84fbfab9ac1a8f4a0ed5a8b8f3dc1eeed3381b8e
Unknown <stheadman1@hotmail.com>
2018-03-06 12:22:18 -0700

    Unity Project

---
+++ UnityGame/CDGameStuff/Assets/Scriptsss/Box.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Box", menuName = "Box")]
+public class Box : ScriptableObject {
+
+    public Vector3 Size;
+    public new string name;
+
+    public string Animation;
+
+    public Color color;
+
+    public int health;
+    public int power;
+
+    public void Print()
+    {
+        Debug.Log(name + " Health: " + health + " " + "Power: " + power);
+    }
+
+}
